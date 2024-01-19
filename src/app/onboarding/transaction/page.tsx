03af31e2c9504a4fb0247130cf0afce3aa09e843
Priest <Charleschukwuemeka47@gmail.com>
2024-01-19 10:25:01 +0100

    Fix: Modularize the code, add new components

--- src/app/onboarding/transaction/page.tsx
+++ src/app/onboarding/transaction/page.tsx
@@ -1,64 +1,11 @@
-"use client"
-import { useState } from "react";
-import Image from "next/image";
-
-//Import Needed Images
-import logo from "../../../../public/Images/logo.svg";
-import arrow from "../../../../public/Images/arrowRight.svg";
-
-//Import Icons
-import { BsEye } from "react-icons/bs";
-import { BsEyeSlash } from "react-icons/bs";
+//Import Needed Components
+import TransactionPin from "@/components/AuthComponents/TransactionPin";
 
 const Page = () => {
-    const [seen, setSeen] = useState<boolean>(false)
-    const [seen1, setSeen1] = useState<boolean>(false)
-
-    const handleSeePassword = () => {
-        setSeen ((prev) => (!prev))
-    }
-
-    const handleSeePassword1 = () => {
-        setSeen1 ((prev) => (!prev))
-    }
-
+   
   return (
-    <main className="h-screen lg:flex">
-      <div className="flex h-full flex-col justify-center px-6 sm:px-8 md:px-10 lg:w-1/2 lg:px-12 xl:px-14 2xl:px-16">
-        <div className="flex gap-x-1">
-          <Image src={logo} alt="Capital Sphere Bank Logo" priority={true} />
-          <div className="text-[#1C1F33] font-semibold text-xs sm:text-sm xl:text-base">
-            <p>Capital</p>
-            <p className="-mt-1">Sphere</p>
-            <p className="-mt-1">Bank</p>
-          </div>
-          </div>
-          <div>
-            <p className="font-bold text-lg sm:text-xl md:text-2xl xl:text-3xl mt-14">
-              Transaction Pin
-            </p>
-            <p className="font-medium text-xs sm:text-sm xl:text-base mt-4">
-                Finish setting up your account, by entering your four(4) digit pin
-            </p>
-          </div>
-        <form action="" className="mt-10">
-            <div className="relative flex flex-col gap-y-1">
-              <label className="cursor-pointer" htmlFor="pin">Pin</label>
-              <input type={seen ? "number" : "password"} maxLength={4} name="pin" id="pin" className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none" placeholder="XXXX"/>
-              <div className="absolute top-[55%] right-4 cursor-pointer text-base sm:text-lg md:text-xl xl:text-2xl" onClick={handleSeePassword}>{seen ? <BsEyeSlash /> : <BsEye />}</div>
-            </div>
-            <div className="relative flex flex-col gap-y-1 mt-6">
-              <label className="cursor-pointer" htmlFor="confirmPin">Confirm Pin</label>
-              <input type={seen1 ? "number" : "password"} maxLength={4} name="confirmPin" id="confirmPin" className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none" placeholder="XXXX"/>
-              <div className="absolute top-[55%] right-4 cursor-pointer text-base sm:text-lg md:text-xl xl:text-2xl" onClick={handleSeePassword1}>{seen1 ? <BsEyeSlash /> : <BsEye />}</div>
-            </div>
-            <button type="submit" className="group mt-14 flex w-full items-center justify-center gap-x-1.5 rounded-md bg-secondary py-2 text-sm text-white sm:text-base md:py-3 lg:text-lg">
-                Finish
-                <Image src={arrow} alt="Right Arrow" className="transition duration-500 group-hover:translate-x-2"/>
-            </button>
-        </form>
-      </div>
-      <div className="createBackground hidden lg:block lg:w-1/2"></div>
+    <main>
+        <TransactionPin />
     </main>
   );
 };
---
+++ src/components/AuthComponents/VerificationForm.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+//Import Needed Components
+import Progress from "@/components/AuthComponents/Progress";
+
+//Import Needed Images
+import logo from "../../../public/Images/logo.svg";
+import arrow from "../../../public/Images/arrowRight.svg";
+
+const VerificationForm = () => {
+    return ( 
+        <main>
+            <div className="flex gap-x-1">
+                <Image src={logo} alt="Capital Sphere Bank Logo" priority={true} />
+                <div className="text-[#1C1F33] font-semibold text-xs sm:text-sm xl:text-base">
+                    <p>Capital</p>
+                    <p className="-mt-1">Sphere</p>
+                    <p className="-mt-1">Bank</p>
+                </div>
+        </div>
+        <div className="text-[#161618] mt-10 mb-4">
+            <p className="text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold">
+              Set up your account
+            </p>
+            <p className="text-xs sm:text-sm xl:text-base font-semibold mt-4">
+              Welcome, let&apos;s get started
+            </p>
+        </div>
+        <Progress activeDiv={3} />
+            <p className="font-bold text-lg sm:text-xl md:text-2xl xl:text-3xl mt-14">
+              Verification
+            </p>
+            <p className="font-medium text-xs sm:text-sm xl:text-base mt-4">
+              You will receive an e-mail OTP. Input OTP to open up your account.
+            </p>
+        <form action="">
+          <div className="flex flex-col gap-y-1 mt-10">
+            <label className="cursor-pointer" htmlFor="otp">
+              OTP
+            </label>
+            <input
+              type="number"
+              name="otp"
+              id="otp"
+              className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none"
+              placeholder="Enter The OTP"
+            />
+          </div>
+          <button
+            type="submit"
+            className="group mt-12 flex w-full items-center justify-center gap-x-1.5 rounded-md bg-secondary py-2 text-sm text-white sm:text-base md:py-3 lg:text-lg"
+          >
+            Continue
+            <Image
+              src={arrow}
+              alt="Right Arrow"
+              className="transition duration-500 group-hover:translate-x-2"
+            />
+          </button>
+        </form>
+    </main>
+);
+}
+ 
+export default VerificationForm;
