5b4e0979513e25b7b3d246f97fd531a450ff5bee
Priest <Charleschukwuemeka47@gmail.com>
2024-01-24 07:31:58 +0100

    Feat: Everything Authentication is done

--- src/components/OnBoardingComponents/TransactionPin.tsx
+++ src/components/OnBoardingComponents/TransactionPin.tsx
@@ -1,64 +1,97 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
 import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import { makeApiRequest } from "@/lib/apiUtils";
+import { toast } from "sonner";
 
 //Import Needed Images
 import logo from "../../../public/Images/logo.svg";
 import arrow from "../../../public/Images/arrowRight.svg";
 
-//Import Icons
-import { BsEye } from "react-icons/bs";
-import { BsEyeSlash } from "react-icons/bs";
 
 const TransactionPin = () => {
+  //Get the email using search Params
+  const searchParams = useSearchParams();
+  const userEmail = searchParams.get("email");
+  const [pin, setPin] = useState("")
+  const router = useRouter();
+  //For the loading state
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const [seen, setSeen] = useState<boolean>(false)
-    const [seen1, setSeen1] = useState<boolean>(false)
+  //OnSubmit Function
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
 
-    const handleSeePassword = () => {
-        setSeen ((prev) => (!prev))
-    }
+    const formData = { email: userEmail, transactionPin : pin };
 
-    const handleSeePassword1 = () => {
-        setSeen1 ((prev) => (!prev))
-    }
+    makeApiRequest("/addTransactionsPin", "post", formData, {
+      onSuccess: () => {
+        // Handle success
+        setLoading(false);
+        toast.success("Transaction pin was added successful");
+        router.push(`/onboarding/review`);
+      },
+      onError: (error: any) => {
+        // Handle error
+        setLoading(false);
+        toast.error("Failed to add transaction pin, please try again later.");
+        router.refresh();
+      },
+    });
+  };
 
-    return ( 
-        <main>
-            <div className="flex gap-x-1">
-                <Image src={logo} alt="Capital Sphere Bank Logo" priority={true} />
-                <div className="text-[#1C1F33] font-semibold text-xs sm:text-sm xl:text-base">
-                    <p>Capital</p>
-                    <p className="-mt-1">Sphere</p>
-                    <p className="-mt-1">Bank</p>
-                </div>
-            </div>
-            <div>
-                <p className="font-bold text-lg sm:text-xl md:text-2xl xl:text-3xl mt-14">
-                  Transaction Pin
-                </p>
-                <p className="font-medium text-xs sm:text-sm xl:text-base mt-4">
-                    Finish setting up your account, by entering your four(4) digit pin
-                </p>
-            </div>
-            <form action="" className="mt-10">
-            <div className="relative flex flex-col gap-y-1">
-              <label className="cursor-pointer" htmlFor="pin">Pin</label>
-              <input type={seen ? "number" : "password"} maxLength={4} name="pin" id="pin" className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none" placeholder="XXXX"/>
-              <div className="absolute top-[55%] right-4 cursor-pointer text-base sm:text-lg md:text-xl xl:text-2xl" onClick={handleSeePassword}>{seen ? <BsEyeSlash /> : <BsEye />}</div>
-            </div>
-            <div className="relative flex flex-col gap-y-1 mt-6">
-              <label className="cursor-pointer" htmlFor="confirmPin">Confirm Pin</label>
-              <input type={seen1 ? "number" : "password"} maxLength={4} name="confirmPin" id="confirmPin" className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none" placeholder="XXXX"/>
-              <div className="absolute top-[55%] right-4 cursor-pointer text-base sm:text-lg md:text-xl xl:text-2xl" onClick={handleSeePassword1}>{seen1 ? <BsEyeSlash /> : <BsEye />}</div>
-            </div>
-            <button type="submit" className="group mt-14 flex w-full items-center justify-center gap-x-1.5 rounded-md bg-secondary py-2 text-sm text-white sm:text-base md:py-3 lg:text-lg">
-                Finish
-                <Image src={arrow} alt="Right Arrow" className="transition duration-500 group-hover:translate-x-2"/>
-            </button>
-        </form>
-        </main>
-     );
-}
- 
+  return (
+    <main>
+      <div className="flex gap-x-1">
+        <Image src={logo} alt="Capital Sphere Bank Logo" priority={true} />
+        <div className="text-[#1C1F33] font-semibold text-xs sm:text-sm xl:text-base">
+          <p>Capital</p>
+          <p className="-mt-1">Sphere</p>
+          <p className="-mt-1">Bank</p>
+        </div>
+      </div>
+      <div>
+        <p className="font-bold text-lg sm:text-xl md:text-2xl xl:text-3xl mt-14">
+          Transaction Pin
+        </p>
+        <p className="font-medium text-xs sm:text-sm xl:text-base mt-4">
+          Finish setting up your account, by entering your four(4) digit pin
+        </p>
+      </div>
+      <form onSubmit={onSubmit} className="mt-10">
+        <div className="flex flex-col gap-y-1">
+          <label className="cursor-pointer" htmlFor="pin">
+            Pin
+          </label>
+          <input
+            type="text"
+            name="pin"
+            id="pin"
+            pattern="\d{4}"
+            title="Please enter exactly 4 digits"
+            className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none"
+            placeholder="XXXX"
+            required
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPin(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="group mt-14 flex w-full items-center justify-center gap-x-1.5 rounded-md bg-secondary py-2 text-sm text-white sm:text-base md:py-3 lg:text-lg"
+        >
+          {loading ? `Adding Pin...` : "Finish"}
+          <Image
+            src={arrow}
+            alt="Right Arrow"
+            className="transition duration-500 group-hover:translate-x-2"
+          />
+        </button>
+      </form>
+    </main>
+  );
+};
+
 export default TransactionPin;
