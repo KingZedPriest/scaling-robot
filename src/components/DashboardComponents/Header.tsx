6926edbb2d8a8979eb1ee7abe8ce88bee628127c
Priest <Charleschukwuemeka47@gmail.com>
2024-01-27 08:27:11 +0100

    Feat: Add the dropdown for the header, so that users can navigate to other parts of the website

--- src/components/DashboardComponents/Header.tsx
+++ src/components/DashboardComponents/Header.tsx
@@ -1,15 +1,37 @@
+"use client"
+import { useState } from "react";
 import { heading } from "@/lib/types";
 import Image from "next/image";
 import Link from "next/link";
+import { LogoutCurve, ProfileCircle } from "iconsax-react";
+
+//Import Needed Icons
+
 
 const Header = ({page, profilePicSrc}: heading) => {
+    //Modal State
+    const [seeModal, setSeeModal] = useState<boolean>(false)
+    //Modal Function
+    const handleModal = () => {
+        setSeeModal((prev) => !prev)
+    }
     return ( 
-        <main className="">
+        <main className="relative">
             <div className="flex justify-between items-center px-4 md:px-6 xl:px-8 py-4 lg:border-b border-[#767680] border-opacity-10">
                 <p className="text-xl xl:text-2xl font-semibold">{page}</p>
-                <Link href="/user/profile"><div className="size-10 md:size-12 xl:size-14 relative cursor-pointer">
+                <div className="size-10 md:size-12 xl:size-14 relative cursor-pointer" onClick={handleModal}>
                     <Image src={profilePicSrc} alt="Profile pic" fill className="absolute rounded-[50%]"/>
-                </div></Link>             
+                </div> 
+                {seeModal && <div className="lg:hidden text-sm p-4 absolute top-[3.5rem] right-10 z-[99] flex flex-col gap-y-5 rounded-md bg-white border border-[#767680] border-opacity-20 shadow">
+                        <Link href="/user/profile" className="flex items-center gap-x-2 text-primary hover:bg-primary hover:text-secondary duration-500 py-2 px-4 cursor-pointer rounded-md">
+                            <ProfileCircle size="20"/>
+                            <p>Profile</p>
+                        </Link>
+                        <Link href="/user/logout" className="flex items-center gap-x-2 text-primary hover:bg-primary hover:text-secondary duration-500 py-2 px-4 cursor-pointer rounded-md">
+                            <LogoutCurve size="20"/>
+                            <p>Logout</p>
+                        </Link>
+                    </div> }          
             </div>
         </main>
      );
