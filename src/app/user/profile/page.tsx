1d73fc662b606473c6a28da12d4e112093502b95
Priest <Charleschukwuemeka47@gmail.com>
2024-02-01 10:56:25 +0100

    Feat: Add the General Details Template for the profiles

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,7 +1,19 @@
-const page = () => {
+import { getUserDetails } from "@/providers/userDetails";
+
+//Import Needed Components
+import Header from "@/components/DashboardComponents/Header";
+import GeneralDetails from "@/components/ProfileComponents/GeneralDetails";
+
+const page = async () => {
+
+    const { user } = await getUserDetails();
+
     return ( 
         <main>
-            
+           <Header page="Profile" profilePicSrc={user?.profileImgSrc}/> 
+           <div>
+            <GeneralDetails />
+           </div>
         </main>
      );
 }
