af16f4404efd85a3a8a2514d064dfa980b553613
Priest <Charleschukwuemeka47@gmail.com>
2024-02-11 13:11:01 +0100

    Fix: Correct vercel error, caused by the account date creation

--- src/components/ProfileComponents/SecurityDetails.tsx
+++ src/components/ProfileComponents/SecurityDetails.tsx
@@ -28,10 +28,6 @@ const SecurityDetails = ({user}: any) => {
                 <p className="text-textPrimary text-[12px] md:text-[14px]">ID Date of expiration</p>
                 <p className="text-[#636366] text-[14px] md:text-[16px] border border-[#E6E7E8] p-2 rounded-lg">{user && user.dateOfExpiry}</p>
             </div>
-            <div className="flex flex-col gap-y-2 mt-6">
-                <p className="text-textPrimary text-[12px] md:text-[14px]">Account Date of creation</p>
-                <p className="text-[#636366] text-[14px] md:text-[16px] border border-[#E6E7E8] p-2 rounded-lg">{user && formatDate(user.createdAt)}</p>
-            </div>
             <div className="flex justify-end mt-10 mb-20 xl:mb-0">
                 <button onClick={updateForm} className="bg-primary px-6 md:px-8 xl:px-10 py-2 md:py-3 text-sm md:text-base rounded-lg text-white border border-primary hover:bg-white hover:text-primary duration-500">Update Details</button>
             </div>
